7b1404d99e983b1a3d571d1943504bc9a205859e
mrsimsek <denizsimsek93@gmail.com>
2020-11-12 11:00:50 +0300

    Added content

--- src/main/java/com/makercodes/configuration/DIConfiguration.java
+++ src/main/java/com/makercodes/configuration/DIConfiguration.java
@@ -2,16 +2,25 @@ package com.makercodes.configuration;
 
 import com.makercodes.service.EmailServiceImpl;
 import com.makercodes.service.MessageService;
+import com.makercodes.service.TwitterServiceImpl;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan(value = {"com.makercodes.consumer"})
 public class DIConfiguration {
 
     @Bean
-    public MessageService getMessageService() {
+    public MessageService getEmailService() {
         return new EmailServiceImpl();
+    }
+
+    @Bean
+    @Primary
+    public MessageService getTwitterService() {
+        return new TwitterServiceImpl();
     }
 }
--- src/main/java/com/makercodes/consumer/MyApplication.java
+++ src/main/java/com/makercodes/consumer/MyApplication.java
@@ -2,6 +2,7 @@ package com.makercodes.consumer;
 
 import com.makercodes.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
